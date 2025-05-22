7ff00d1f735c4e79d7a34b0b63b9e08018ac60d6
MattWellie <mattwellie@gmail.com>
2025-05-22 12:19:07 +1000

    meh

--- src/cpg_flow_gatk_sv/jobs/EvidenceQC.py
+++ src/cpg_flow_gatk_sv/jobs/EvidenceQC.py
@@ -28,7 +28,7 @@ def create_evidence_qc_jobs(
 
     sgids = cohort.get_sequencing_group_ids()
 
-    input_dict: dict = {
+    cromwell_input_dict: dict = {
         'batch': cohort.id,
         'samples': sgids,
         'run_vcf_qc': True,
@@ -36,19 +36,19 @@ def create_evidence_qc_jobs(
     }
 
     for caller in utils.SV_CALLERS:
-        input_dict[f'{caller}_vcfs'] = [str(input_dict[sid][f'{caller}_vcf']) for sid in sgids]
+        cromwell_input_dict[f'{caller}_vcfs'] = [str(input_dict[sid][f'{caller}_vcf']) for sid in sgids]
 
-    input_dict |= utils.get_images(
+    cromwell_input_dict |= utils.get_images(
         ['sv_base_mini_docker', 'sv_base_docker', 'sv_pipeline_docker', 'sv_pipeline_qc_docker'],
     )
 
-    input_dict |= utils.get_references(['genome_file', 'wgd_scoring_mask'])
+    cromwell_input_dict |= utils.get_references(['genome_file', 'wgd_scoring_mask'])
 
     # runs for approx 5 hours, depending on sample count
     jobs = utils.add_gatk_sv_jobs(
         dataset=cohort.dataset,
         wfl_name='EvidenceQC',
-        input_dict=input_dict,
+        input_dict=cromwell_input_dict,
         expected_out_dict=output_dict,
         labels={'stage': 'EvidenceQC', AR_GUID_NAME: try_get_ar_guid()},
         job_size=utils.CromwellJobSizes.MEDIUM,
