6982c9a05e57d08db02b57194f395762978164e9
MattWellie <mattwellie@gmail.com>
2025-05-24 12:00:28 +1000

    Train gCNV

--- src/cpg_flow_gatk_sv/second_workflow.py
+++ src/cpg_flow_gatk_sv/second_workflow.py
@@ -3,8 +3,9 @@ All post-batching stages of the GATK-SV workflow
 """
 
 from cpg_utils import Path
-from cpg_flow import stage, targets, workflow
+from cpg_flow import stage, targets
 from cpg_flow_gatk_sv.utils import check_for_cohort_overlaps, make_combined_ped
+from cpg_flow_gatk_sv.jobs import TrainGCNV
 
 
 @stage.stage
@@ -34,3 +35,30 @@ class MakeMultiCohortCombinedPed(stage.MultiCohortStage):
         make_combined_ped(multicohort, output)
 
         return self.make_outputs(target=multicohort, data=output)
+
+
+@stage.stage(required_stages=[MakeCohortCombinedPed])
+class TrainGcnvStage(stage.CohortStage):
+    """
+    Runs TrainGCNV to generate a model trained on a selection of samples within this Cohort, used in GatherBatchEvidence
+    run https://github.com/broadinstitute/gatk-sv/blob/main/wdl/TrainGCNV.wdl
+    config: https://github.com/broadinstitute/gatk-sv/blob/main/inputs/templates/terra_workspaces/cohort_mode/workflow_configurations/TrainGCNV.json.tmpl
+    """
+
+    def expected_outputs(self, cohort: targets.Cohort) -> dict[str, Path]:
+        return {
+            'cohort_contig_ploidy_model_tar': self.get_stage_cohort_prefix(cohort)
+            / 'cohort_contig_ploidy_model.tar.gz',
+            'cohort_contig_ploidy_calls_tar': self.get_stage_cohort_prefix(cohort)
+            / 'cohort_contig_ploidy_calls.tar.gz',
+            'cohort_gcnv_model_tars': [
+                self.get_stage_cohort_prefix(cohort) / 'cohort_gcnv_model.tar.gz'
+            ],  # I have absolutely no idea how long this will be
+        }
+
+    def queue_jobs(self, cohort: targets.Cohort, inputs: stage.StageInput) -> stage.StageOutput:
+        outputs = self.expected_outputs(cohort)
+
+        jobs = TrainGCNV.add_train_gcnv_jobs(cohort, output_dict=outputs)
+
+        return self.make_outputs(cohort, data=outputs, jobs=jobs)
