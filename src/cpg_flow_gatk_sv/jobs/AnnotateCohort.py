893704377f5abc3394bdc2fe019c1c96721770ca
MattWellie <mattwellie@gmail.com>
2025-07-03 09:07:15 +1000

    Config bumps, argument corrections

--- src/cpg_flow_gatk_sv/jobs/AnnotateCohort.py
+++ src/cpg_flow_gatk_sv/jobs/AnnotateCohort.py
@@ -12,15 +12,15 @@ def create_annotate_cohort_job(
     out_mt: Path,
     job_attrs: dict,
 ) -> 'BashJob':
-    job = hail_batch.get_batch().new_job('Annotate cohort', job_attrs)
+    job = hail_batch.get_batch().new_bash_job('Annotate cohort', job_attrs)
     job.image(config.config_retrieve(['workflow', 'driver_image']))
     gencode_gtf_local = hail_batch.get_batch().read_input(config.config_retrieve(['workflow', 'gencode_gtf_file']))
     job.storage('10Gi')
     job.command(f"""
         python -m cpg_flow_gatk_sv.scripts.annotate_cohort \
         --vcf {vcf!s}
-        --out_mt {out_mt!s}
+        --output {out_mt!s}
         --gencode_gtf {gencode_gtf_local}
-        --checkpoint_prefix {checkpoint!s}
+        --checkpoint {checkpoint!s}
     """)
     return job
