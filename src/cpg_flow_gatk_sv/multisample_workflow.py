c1ca7591fd6d9430d0c7d73039bfdb5fbcdd5b81
Johnnyassaf <117962983+Johnnyassaf@users.noreply.github.com>
2026-05-22 12:00:25 +1000

    registering-svmts (#46)
    Co-authored-by: Matt Welland <mattwellie@gmail.com>
    
    Registering SV matrixtable outputs for the multisample workflow in metamist
    version bump 0.1.28 -> 0.1.29

--- src/cpg_flow_gatk_sv/multisample_workflow.py
+++ src/cpg_flow_gatk_sv/multisample_workflow.py
@@ -790,7 +790,7 @@ class SpiceUpSvIds(stage.MultiCohortStage):
         return self.make_outputs(multicohort, data=output, jobs=jobs)
 
 
-@stage.stage(required_stages=SpiceUpSvIds)
+@stage.stage(required_stages=SpiceUpSvIds, analysis_type='sv')
 class AnnotateCohort(stage.MultiCohortStage):
     """
     First step to transform annotated SV callset data into a seqr ready format
